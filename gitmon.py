4b3dd3da9154d6316d122625a814996a4318e5de
Tomas Varaneckas <tomas.varaneckas@thenbt.com>
2010-09-29 21:13:58 +0300

    further development

--- gitmon.py
+++ gitmon.py
@@ -7,6 +7,19 @@ Created by Tomas Varaneckas on 2010-09-26.
 Copyright (c) 2010 Tomas Varaneckas. All rights reserved.
 """
 import os
+import sys
+from git import *
+
+class Repository:
+    def __init__(self, name, path):
+        self.repo = Repo(path)
+        self.name = name
+        self.path = path
+    def check_status(self):
+        if verbose: 
+            print 'Checking repo: %s' % self.name
+        info = self.repo.remotes.origin.fetch()
+        print '%s %s' % (info, dir(info))
 
 class Gitmon:
     
@@ -19,7 +32,7 @@ class Gitmon:
             config.write('test')
             config.close()
         else:
-            print 'loading configuration from %s' % self.conf_file
+            print 'Loading configuration from %s' % self.conf_file
             with open(self.conf_file) as conf:
                 for line in conf:
                     if not line.strip().startswith('#') and line.strip() != '':
@@ -29,18 +42,36 @@ class Gitmon:
                             pair = line.strip().split('=', 1)
                             self.config[pair[0].strip()] = pair[1].strip()
                         
+    def load_repos(self):
+        for r in self.config['monitor'].split(','):
+            r = r.strip()
+            name = self.config[r + '.name']
+            path = self.config[r + '.path']
+            print 'Tracking repo: "%s" at %s' % (name, path)
+            self.repos.append(Repository(name, path))    
     
     def __init__(self):
         self.config = {}
+        self.repos = []
         self.conf_file = os.getenv('GITMON_CONF', '~/.gitmon.conf')
         self.conf_file = os.path.expanduser(self.conf_file)
         self.load_config()
-        print 'loaded config: %s' % self.config
+        self.load_repos()
+        if verbose:
+            print 'Loaded config: %s' % self.config     
+            
+    def check(self):
+        for repo in self.repos:
+            repo.check_status()   
         
-        
+verbose = False        
         
 def main():
+    global verbose
+    program_name, args = sys.argv[0], sys.argv[1:]
+    verbose = '-v' in args
     app = Gitmon()
+    app.check()
 
 if __name__ == '__main__':
 	main()
