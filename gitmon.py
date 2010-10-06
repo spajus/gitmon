99362c330e650c18634cfc7d5e5aa47932316f97
Tomas Varaneckas <tomas.varaneckas@thenbt.com>
2010-10-06 07:20:13 +0300

    Possibility to get more than one commit in updates. Better status display.

--- gitmon.py
+++ gitmon.py
@@ -13,6 +13,23 @@ import subprocess
 import re    
 from git import *
 
+#Current version. Print with --version when running
+version = 0.1
+#Should gitmon produce verbose output? Override with -v when running.
+verbose = False 
+#Should gitmon notify when new branch is created? Set in config.
+notify_new_branch = False   
+#Should debug output be printed? Override with --debug when running.
+debug = False    
+#Should updates be pulled automatically?
+auto_pull = False
+#How many latest commits to display?
+max_last_commits = 5
+
+def dump(obj):
+    for attr in dir(obj):
+        print "obj.%s = %s" % (attr, getattr(obj, attr))
+
 class Repository:
     """Works with GitPython's to produce nice status update information"""
     
@@ -33,7 +50,7 @@ class Repository:
         #get last commits in current remote ref
         local_commits = {}
         for rem in self.repo.remotes.origin.refs:
-            local_commits[rem.remote_head] = rem.object
+            local_commits[rem.remote_head] = rem.commit
         
         try:
             #fetch new data
@@ -43,50 +60,75 @@ class Repository:
                 if hasattr(fi.ref, 'remote_head'):
                     branch = fi.ref.remote_head       
                 else:
-                    #this is probably a tag, let's skip it
+                    #this is probably a tag, let's skip it for now
                     continue
-                try: #sometimes retrieval of remote commit fails
+                try: #sometimes reference may not exist
                     remote_commit = fi.commit
-                except Exception:
+                except Exception as e:
+                    print 'Could not fetch %s (%s): %s' % (self.name, branch, e)
                     continue
                 if local_commits.has_key(branch):
                     local_commit = local_commits[branch]
-                    up = self.compare_commits(local_commit, remote_commit)
-                    if up:
-                        up.branch = branch
+                    ups = self.compare_commits(local_commit, remote_commit)
+                    if ups:
+                        up = UpdateStatus(branch)
+                        up.add(ups)
                         updates.append(up)
                 else:
                     if notify_new_branch:
+                    #FIXME update it
                         updates.append(
                             UpdateStatus(remote_commit.message, 
                                 'NEW %s' % branch, remote_commit.author))
             if auto_pull:
                 try:
                     self.repo.remotes.origin.pull()
-                except Exception:
+                except Exception as e:
                     if verbose:
-                        print 'Failed pulling repo: %s' % repo.name
+                        print 'Failed pulling repo: %s, %s' % (self.name, e)
             return updates
         except AssertionError as e:
             if verbose:
                 print 'Failed checking for updates: %s' % self.path
+                dump(e)
 
-    def compare_commits(self, local, remote):
-        """Compares local and remote commits to produce UpdateStatus
+    def compare_commits(self, local, remote, depth=1, updates=None):
+        """Compares local and remote commits to produce list of Update
         if remote commit is newer"""
         if local.hexsha == remote.hexsha:
-            return None
+            return updates
         if local.committed_date < remote.committed_date:
-            return UpdateStatus(remote.message, author=remote.author)
+            if not updates:
+                updates = []
+            updates.append(Update(remote))
+            if remote.parents and depth < max_last_commits:
+                self.compare_commits(local, remote.parents[0], depth + 1, updates)
+            return updates
+            
 
 class UpdateStatus:
     """Contains status update information which is displayed in user 
     notification"""
     
-    def __init__(self, message, branch=None, author=None):
-        self.author = author
-        self.message = message
+    def __init__(self, branch=None):
         self.branch = branch
+        self.updates = []
+
+    def add(self, update):
+        self.updates.extend(update)
+
+    def __str__(self):
+        print 'In %s:\n%s' % (self.branch, '\n'.join(self.updates))
+
+class Update:
+    
+    def __init__(self, commit):
+        self.message = commit.message
+        self.author = commit.committer.name
+        self.stats = commit.stats.total
+
+    def __str__(self):
+       print '%s: %s (%s)' % (self.author, self.message, self.stats) 
 
 class Gitmon:
     """Handles the big picture - config loading, checking for updates"""
@@ -153,11 +195,7 @@ class Gitmon:
         for repo in self.repos:
             st = repo.check_status() 
             if st:
-                mess = []
-                for up in st:
-                    mess.append('%s in %s:\n %s' % \
-                                        (up.author, up.branch, up.message))
-                self.notify(repo, '\n'.join(mess))
+                self.notify(repo, '\n'.join(st))
             
     def notify(self, repo, message):        
         """Notifies user about status updates with notification.command 
@@ -174,16 +212,6 @@ class Gitmon:
         if retcode != 0:
             print 'Error while notifying: %s, %s' % (retcode, args)
        
-#Should gitmon produce verbose output? Override with -v when running.
-verbose = False 
-#Should gitmon notify when new branch is created? Set in config.
-notify_new_branch = False   
-#Should debug output be printed? Override with --debug when running.
-debug = False    
-#Current version. Print with --version when running
-version = 0.1
-#Should updates be pulled automatically?
-auto_pull = False
         
 def main():
     global verbose, debug
