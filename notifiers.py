bbc51632bd55a74a8bd89801633b076a297d8927
Tomas Varaneckas <tomas.varaneckas@thenbt.com>
2010-10-17 13:24:39 +0300

    Fixing wrong class name

--- notifiers.py
+++ notifiers.py
@@ -18,15 +18,30 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 """
 
 class Notifier(object):
-    def notify(self, updates):
+    def notify(self, title=None, message=None, image=None):
         pass
 
     @classmethod
     def create(cls, type):
         if type == 'command.line':
-            return SystemCommandNotifier()
+            return CommandLineNotifier()
 
 class CommandLineNotifier(Notifier):
-    def notify(self, updates):
-        print 'Notifying'
-
+    def notify(self, title, message, image):
+        notif_cmd = self.config['notification.command'].split(' ')
+        if '${message}' in notif_cmd:
+            notif_cmd[notif_cmd.index('${message}')] = message.strip()
+        if '${title}' in notif_cmd:
+            notif_cmd[notif_cmd.index('${title}')] = '%s\n%s' % \
+                                                    (repo.name, repo.path)
+        if '${image}' in notif_cmd:
+            notif_cmd[notif_cmd.index('${image}')] = gitmon_dir + '/git.png' 
+        self.exec_notification(notif_cmd, repo.path_full)
+       
+    def exec_notification(self, notif_cmd, path):
+        """Does the actual execution of notification command"""
+        proc = subprocess.Popen(notif_cmd, cwd=path, stdout=subprocess.PIPE)
+        output, _ = proc.communicate()
+        retcode = proc.wait()        
+        if retcode != 0:
+            print 'Error while notifying: %s, %s' % (retcode, args)
