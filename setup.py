073ef2edca9b56446ef8a867cacdcd21e0131813
Tomas Varaneckas <tomas.varaneckas@thenbt.com>
2010-11-05 16:56:48 +0200

    Tidying up the code

--- setup.py
+++ setup.py
@@ -17,17 +17,17 @@ setup(
     data_files = ['misc/gitmon.conf.example'],
 
     zip_safe = False,
-    include_package_data = True,
 
+    package_data = {'': ['*.png', '*.example']},
     requires = ('gitpython (>=0.2.9)', 'py_Growl (>=0.0.7)'),
     install_requires = ['gitpython >= 0.2.9', 'py_Growl >= 0.0.7'],
 
     #Metadata for PyPI
+    url = 'http://github.com/spajus/gitmon/',
     author = 'Tomas Varaneckas',
     author_email = 'tomas.varaneckas@gmail.com',
     description = 'GitMon - The Git Repository Monitor',
     long_description = 'GitMon - The Git Repository Monitor',
     license = 'GPLv3',
-    keywords = ['git', 'monitor', 'scm', 'repository'],
-    url = 'http://github.com/spajus/gitmon/'
+    keywords = ['git', 'monitor', 'scm', 'repository']
 )
