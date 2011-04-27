c0a25ace1470cc40972b082f2ec276cb9a0ac749
Tomas Varaneckas <tomas.varaneckas@onegoteam.com>
2011-04-27 08:03:35 +0300

    Version bump up

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ except ImportError:
 
 setup(
     name = 'GitMon',
-    version = '0.3.2',
+    version = '0.3.3',
     packages = find_packages('lib'),
     package_dir = {'' : 'lib'},
     scripts = ['misc/gitmon'],
@@ -29,7 +29,7 @@ setup(
     license = 'GPLv3',
     keywords = ['git', 'monitor', 'scm', 'repository'],
     classifiers = [
-        'Development Status :: 4 - Beta', 
+        'Development Status :: 4 - Beta',
         'Environment :: MacOS X',
         'License :: OSI Approved :: GNU General Public License (GPL)',
         'Operating System :: OS Independent',
